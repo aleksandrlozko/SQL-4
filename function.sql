11e7ac344238865ea8e3cd8cc6545bc7873d9ea7
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-05-25 00:11:02 +0300

    Create function.sql

---
+++ function.sql
@@ -0,0 +1,24 @@
+CREATE TYPE movie1 AS OBJECT (
+    movie_director VARCHAR2(60)
+)
+/
+CREATE TYPE movie_type IS TABLE OF movie1;
+/
+CREATE OR REPLACE FUNCTION get_title (
+    t_duration movie.time_duration%type,
+    r_year movie.release_year%type
+) RETURN movie_type
+AS
+    result_movie movie_type := movie_type();
+BEGIN
+    for cursor IN (
+                    SELECT title
+                    FROM movie
+                    WHERE time_duration = t_duration AND release_year = r_year
+                )
+    LOOP
+        result_movie.extend;
+        result_movie(result_movie.last) := movie1(cursor.title);              
+    END LOOP;
+    RETURN result_movie;
+END;
