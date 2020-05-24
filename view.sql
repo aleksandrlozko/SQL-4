314f44951ba1542b6780923c847d208b52e38f59
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-05-25 00:56:31 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,8 +1,8 @@
 CREATE VIEW view_movie AS
 SELECT
-movie.title,
-movie_country.country_country,
-movie_director.director_director
+movie.title as name,
+movie_country.country_country as country,
+movie_director.director_director AS director
 FROM movie
     JOIN movie_country
     ON movie.title = movie_country.movie_title
