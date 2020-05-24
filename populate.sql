5f4b8e22040eb58cddfa0b70d85d89140a6d5f35
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-05-25 00:11:55 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,4 +1,4 @@
-INSERT INTO country (country) VALUES ('Ukraine');
+IINSERT INTO country (country) VALUES ('Ukraine');
 INSERT INTO country (country) VALUES ('UK');
 INSERT INTO country (country) VALUES ('USA');
 
@@ -10,9 +10,9 @@ INSERT INTO actor (name) VALUES ('Michael');
 INSERT INTO actor (name) VALUES ('Eva');
 INSERT INTO actor (name) VALUES ('Alex');
 
-INSERT INTO movie (title) VALUES ('American Horror Story');
-INSERT INTO movie (title) VALUES ('Elite');
-INSERT INTO movie (title) VALUES ('Scream Queens');
+INSERT INTO movie (title, time_duration, release_year) VALUES ('American Horror Story', 70, 2010);
+INSERT INTO movie (title, time_duration, release_year) VALUES ('Elite', 120, 2015);
+INSERT INTO movie (title, time_duration, release_year) VALUES ('Scream Queens', 100, 2017);
 
 INSERT INTO movie_country (country_country, movie_title) VALUES ('Ukraine', 'American Horror Story');
 INSERT INTO movie_country (country_country, movie_title) VALUES ('UK', 'Elite');
