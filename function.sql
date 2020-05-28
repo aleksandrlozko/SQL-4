6200a4bc8179be8575204fcffeb389cded940800
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-05-29 02:42:38 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,17 +1,14 @@
-CREATE TYPE movie1 AS OBJECT (
-    movie_director VARCHAR2(60)
-)
+CREATE TYPE movie1 AS OBJECT (movie_director VARCHAR2(60))
 /
 CREATE TYPE movie_type IS TABLE OF movie1;
 /
-CREATE OR REPLACE FUNCTION get_title (
-    t_duration movie.time_duration%type,
-    r_year movie.release_year%type
-) RETURN movie_type
-AS
-    result_movie movie_type := movie_type();
+CREATE OR REPLACE FUNCTION get_title (t_duration movie.time_duration%type,
+r_year movie.release_year%type) 
+RETURN movie_type AS
+result_movie movie_type := movie_type();
+                                                      
 BEGIN
-    for cursor IN (
+    FOR cursor IN (
                     SELECT title
                     FROM movie
                     WHERE time_duration = t_duration AND release_year = r_year
